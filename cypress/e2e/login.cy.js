d96de1b9c3ba2b9106a635ca316a8f06310534c7
Ricardo Montanari <montanari.rc@gmail.com>
2025-01-04 15:28:52 -0300

    alterações do primeiro teste do projeto

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Testes de login do site', () => {
+  beforeEach(() => {
+    cy.visit('/')
   })
+
+  before(function (){
+    // disable Cypress's default behavior of logging all XMLHttpRequests and fetches to the Command Log
+    cy.intercept({ resourceType: /xhr|fetch/ }, {log: false})
+  })
+  it('Validar o login do usuário padrão com sucesso', () => {
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+
+    cy.get('.shopping_cart_link').should('be.visible')
+  })
+  
 })
